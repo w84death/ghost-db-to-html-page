693f379985953e2dcbfe1dca3481b0ee7730bcaa
Krzysztof Krystian Jankowski <kj@p1x.in>
2023-07-01 15:54:53 +0200

    working script

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# ghost-db-to-html-page
-Extractor of data (pages) from ghost blog SQLite db to a static HTML.
+# Ghost Blog Database Extractor
+
+This Python script extracts posts from a Ghost blog SQLite database and generates individual HTML pages for each post. It also creates an index page with a list of links to the extracted posts. The extracted pages can be used for offline browsing or to migrate content to another platform.
+
+## Requirements
+
+- Python 3.x
+- SQLite3 library (included in Python standard library)
+- Jinja2 library (can be installed via `pip install Jinja2`)
+
+## Usage
+
+1. Ensure you have a backup of your Ghost blog database in SQLite format (`database.db`).
+
+2. Place the script in the same directory as the backup database.
+
+3. Open a terminal or command prompt and navigate to the script directory.
+
+4. Run the script with the following command:
+
+```python extract_posts.py ../path/to/ghost.db```
+
+Replace `../path/to/ghost.db` with the actual name of your SQLite database.
+
+5. The script will create an `out` directory (if it doesn't already exist) and generate individual HTML pages for each post inside the `out` directory.
+
+6. It will also generate an `index.html` file in the `out` directory, which contains a list of links to the extracted posts.
+
+7. To preview the generated pages run the following command:
+
+```python -m http.server -d out```
+
+This will start a local web server. Open your web browser and visit `http://localhost:8000` to view the extracted pages.
+
+8. You can customize the HTML templates for individual posts and the index page by modifying the respective template files (`template.html` and `index_template.html`). Make sure to update the script with the correct paths to these template files.
+
+## Notes
+
+- Ensure that the necessary images, or any other external assets referenced in the HTML templates are also included in the appropriate directories within the `out` directory for proper rendering.
+
+- The script uses the Jinja2 template engine to render the HTML templates. Please refer to the Jinja2 documentation (https://jinja.palletsprojects.com/) for more information on template syntax and features.
+
+- This script assumes a certain database schema for Ghost blog. If your database schema is different, you may need to modify the script accordingly.
+
+- Always make a backup of your database before running the script to avoid any accidental data loss.
+
+- This script and description was made with a help of ChatGPT
---
+++ extract_posts.py
@@ -0,0 +1,105 @@
+import sqlite3
+from jinja2 import Template
+import datetime
+import re
+import os
+import sys
+
+# Check if the output directory exists, create it if necessary
+output_dir = 'out'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Get the path to the SQLite database from command-line argument
+if len(sys.argv) < 2:
+    sys.exit("Please provide the path to the SQLite database as a command-line argument.")
+db_path = sys.argv[1]
+
+print(f"Extracting ghost blog from {db_path}..")
+
+# Connect to the SQLite database
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+# Fetch page data from settings
+keys = ['og_title', 'description', 'cover_image']
+page_data = {}
+
+for key in keys:
+    cursor.execute("SELECT value FROM settings WHERE key=?", (key,))
+    value = cursor.fetchone()
+    if value:
+        page_data[key] = value[0]
+    else:
+        page_data[key] = ""
+
+# Extract and list posts
+cursor.execute("SELECT title, slug, html, created_at FROM posts")
+posts = cursor.fetchall()
+
+# HTML template for individual post pages
+post_template = Template(open('page_template.html').read())
+
+# HTML template for the index page
+index_template = Template(open('index_template.html').read())
+
+# Function to clean the post HTML
+def clean_html(post_html):
+    # Remove instances of '__GHOST_URL__/'
+    cleaned_html = post_html.replace('__GHOST_URL__/', '')
+
+    # Remove 'class' instances with parameters
+    cleaned_html = re.sub(r'class=".*?"', '', cleaned_html)
+
+    # Strip <img> tags from everything except 'src'
+    cleaned_html = re.sub(r'<img((?!src=).)*>', '', cleaned_html)
+
+    return cleaned_html
+
+# Generate individual HTML pages for each post
+for post in posts:
+    post_title = post[0]
+    post_slug = post[1]
+    post_html = post[2]
+    post_created_at = post[3]
+
+    # Format the post creation date
+    created_at = datetime.datetime.strptime(post_created_at, "%Y-%m-%d %H:%M:%S")
+
+    # Clean the post HTML
+    cleaned_html = clean_html(post_html)
+
+    # Render the HTML template with the cleaned post data
+    rendered_html = post_template.render(title=post_title, content=cleaned_html, created_at=created_at)
+
+    # Write the rendered HTML to a file in the output directory
+    file_name = os.path.join(output_dir, f'{post_slug}.html')
+    with open(file_name, 'w') as file:
+        file.write(rendered_html)
+
+# Extract page data
+page_title = page_data['og_title']
+page_description = page_data['description']
+page_cover_image = page_data['cover_image']
+
+# Clean the page cover image URL
+cleaned_cover_image = page_cover_image.replace('__GHOST_URL__/', '')
+
+# Render the HTML template with page data
+index_html = index_template.render(title=page_title, description=page_description, cover_image=cleaned_cover_image, posts=posts)
+
+# Write the index HTML to a file in the output directory
+index_file = os.path.join(output_dir, 'index.html')
+with open(index_file, 'w') as file:
+    file.write(index_html)
+
+# Close the database connection
+conn.close()
+
+# Print extraction summary
+num_pages = len(posts)
+print(f"Extraction completed. {num_pages} pages extracted.")
+
+# Provide closing message with preview command
+print(f"\nTo preview the generated pages run the following command:\n")
+print("$ python -m http.server -d out\n")
